149659f1ca36baa1a0ee37a435ca435ed5f42d78
Thendo Ramurafhi <thendoramurafhi08@outlook.com>
2024-06-28 14:35:45 +0200

    update the Vehicle owners page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Welcome from './Welcome';
 import Login from './Login';
-import VehicleOnwer from './VehicleOnwer';
+import VehicleOwner from './VehicleOwner';
 
 function App() {
   return (
     <div className="App">
     
-      <VehicleOnwer />
+      <VehicleOwner />
     </div>
   );
 }
