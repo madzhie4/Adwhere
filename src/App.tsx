434617355cf75fac926e0c7a26b60cf87605bb0e
Tshilidzi <tshilidzi@Mashudus-Air.lan>
2024-07-05 20:16:07 +0200

    Update App file main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-};
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './Welcome';
+import Signup from './Signup';
+import Profile from './Profile';
+import Welcome from './Welcome';
+import Login from './Login';
+import ClientRegistation from './ClientRegistation';
+import VehicleOwner from './VehicleOwner';
+import ClientApplicationForm from './ClientApplicationForm';
 
-export default App;
+function MainApp() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/ClientRegistation" element={<ClientRegistation />} />
+        <Route path="/VehicleOwner" element={<VehicleOwner />} />
+        <Route path="/ClientApplicationForm" element={<ClientApplicationForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+export default MainApp;
